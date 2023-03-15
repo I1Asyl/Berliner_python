86e5ad84256e38808ad16058682b8ab642aca7fa
Yerassyl Altay <altayyerassyl@gmail.com>
2023-03-15 14:27:13 +0100

    This commit was automatically pushed from Yerassyl Altay's device on 03/15/2023.

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Group(models.Model):
     groupName = models.CharField(max_length=50, default='unnamed group')
     groupSize = models.IntegerField(default=1)
+    groupOwner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.groupName
 # Create your models here.
