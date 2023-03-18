caa8ca92c5a1d2b7b367039f27a0308e116becf5
Yerassyl Altay <altayyerassyl@gmail.com>
2023-03-18 09:26:42 +0100

    This commit was automatically pushed from Yerassyl Altay's device on 03/18/2023.

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'accounts'
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +52,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'helloworld_project.urls'
+
+
+
 
 TEMPLATES = [
     {
