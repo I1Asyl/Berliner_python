8da837181a4f309062b1fb73f9ef877a9fd935b8
Yerassyl Altay <altayyerassyl@gmail.com>
2023-03-25 14:10:43 +0100

    This commit was automatically pushed from Yerassyl Altay's device on 03/25/2023.

--- users/models.py
+++ users/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(default=0)
+    name = models.CharField(default="", max_length=20)
+    surname = models.CharField(default="", max_length=20)
